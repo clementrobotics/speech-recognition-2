121641abc33909f38be76afcca4e666a0c4627e7
Joseph Barrow <joseph.d.barrow@gmail.com>
2013-11-04 22:22:33 -0500

    Worked on get_ngrams

--- main.py
+++ main.py
@@ -4,6 +4,12 @@
 
 import re, collections
 
+# TODO: Remove non-word characters from the list of tokens
+# TODO: Define a data structure for our HMM and for getting
+#		the probabilities
+# TODO: Define a data structure for part-of-speech tagged
+#		text
+
 class HMM:
 	def train(self):
 		"""
@@ -32,12 +38,27 @@ def extract_sentences(*files):
 		# a file. It has a look-behind to match for common uses
 		# of the period, and a look-ahead to ensure that the
 		# next sentence starts with a capital letter or a number.
-		sentences.append([sentence.strip() for sentence in re.split(r"(?<!\s)(?<!\be\.g|\bi\.e)(?<!\b[Mm]r|\b[Mm]s|\b[Dd]r|\bvs|\.\.)(?<!\b[Mm]rs)(?:\.|\!|\?)(?=\s+[A-Z0-9\(\)])", file(file_name).read())])
+		sentences.extend([sentence.strip() for sentence in re.split(r"(?<!\s)(?<!\be\.g|\bi\.e)(?<!\b[Mm]r|\b[Mm]s|\b[Dd]r|\bvs|\.\.)(?<!\b[Mm]rs)(?:\.|\!|\?)(?=\s+[A-Z0-9\(\)])", file(file_name).read())])
 		# I would just like to note that the above line of code
 		# is what happens when I program while tired.
 	return sentences
 
 def get_ngrams(sentences, n):
-	print(sentences)
+	"""
+	Get an n-gram for a given array of sentences after first
+	standardizing them with an initial and post character.
+	"""
+	assert n >= 1, "You must use at least a unigram"
+
+	for sentence in sentences:
+		tokens = sentence.split(" ")
+		# We want to insert the * * ... STOP for the sentence
+		for _ in range(n-1):
+			tokens.insert(0, "*")
+		tokens.extend(["STOP"])
+		for index, token in enumerate(tokens):
+			if index >= n:
+				print token
+		
 
 get_ngrams(extract_sentences('test.txt'), 3)
